9c09cf3c9922e71bd78c26154732ba92bf99b576
Thomas Perone <tvp3189@log-1.hpc.nyu.edu>
2023-11-16 15:50:20 -0500

    Ran equil and prod, started analysis script

---
+++ Week9/Data/create_anneal.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16GB
+#SBATCH --time=2:00:00
+#SBATCH --job-name=W9pt1
+#SBATCH --mail-type=END
+#SBATCH --mail-user=tvp3189@nyu.edu
+#SBATCH --output=slurm_%j.out
+
+source /scratch/work/courses/CHEM-GA-2671-2023fa/software/lammps-gcc-30Oct2022/setup_lammps.bash
+
+# Create the system
+mpirun lmp -var configfile ../Inputs/n360/kalj_n360_create.lmp -var id 1 -in ../Inputs/create_3d_binary.lmp
+
+# Cool from 1.5 to 0.45
+for TEMP in 1.5 1.0 0.9 0.8 0.7 0.65 0.6 0.55 0.5 0.475
+do
+    mpirun lmp -var configfile ../Inputs/n360/kalj_n360_T${TEMP}.lmp -var id 1 -in ../Inputs/anneal_3d_binary.lmp
+done
+
